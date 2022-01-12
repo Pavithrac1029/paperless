ee9f7d9ee82f14071d67815843af1628ad3c66a7
aswin <aswinsaikumar_jeganmohan@cviac.com>
2022-01-12 17:49:14 +0530

    login and otp changes

--- src/app/otp-screen/otp-screen.page.ts
+++ src/app/otp-screen/otp-screen.page.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-otp-screen',
   templateUrl: './otp-screen.page.html',
   styleUrls: ['./otp-screen.page.scss'],
+  providers: [Keyboard]
 })
 export class OtpScreenPage implements OnInit {
 
-  constructor() {
+  constructor(public navctrl:NavController) {
     
    }
 
   ngOnInit() {
   }
- 
-
+  back(){
+    this.navctrl.navigateRoot("/login")
+  }
 }
