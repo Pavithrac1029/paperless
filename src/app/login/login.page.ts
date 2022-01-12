7053c55edec83ddc21227def913c86c07bab28a0
aswin <aswinsaikumar_jeganmohan@cviac.com>
2022-01-12 10:23:23 +0530

    changes

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Keyboard } from '@ionic-native/keyboard/ngx';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  providers: [Keyboard]
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  isKeyboardHide:boolean = true;
+  constructor(private keyboard:Keyboard) { }
 
   ngOnInit() {
   }
+  ionViewWillEnter() {
+    this.keyboard.onKeyboardWillShow().subscribe(()=>{
+      this.isKeyboardHide=false;
+      // console.log('SHOWK');
+    });
 
+    this.keyboard.onKeyboardWillHide().subscribe(()=>{
+      this.isKeyboardHide=true;
+      // console.log('HIDEK');
+    });
+  }
 }
