a4d92aaad8c93f32869f01eb01f7b65ce1677898
pavithrac1029 <pavithra_chandramohan@cviac.com>
2022-01-12 04:21:08 -0800

    pull changes

--- src/app/otp-screen/otp-screen.page.ts
+++ src/app/otp-screen/otp-screen.page.ts
@@ -6,13 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./otp-screen.page.scss'],
 })
 export class OtpScreenPage implements OnInit {
-
+  OTP: string = '';
   constructor() {
     
    }
 
   ngOnInit() {
   }
- 
+  otpController(event,next,prev, index){
+
+
+    if(index == 6) {
+      console.log("submit")
+    }
+    if(event.target.value.length < 1 && prev){
+      prev.setFocus()
+    }
+    else if(next && event.target.value.length>0){
+      next.setFocus();
+    }
+    else {
+     return 0;
+    } 
+ }
 
 }
